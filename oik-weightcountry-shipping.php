65395ade1aac8ab28add19a48b5c1ce36a4e05ed
Christopher Pax <christopher.pax@gmail.com>
2015-04-19 15:08:15 +0800

    Changed method back to HKP.

--- oik-weightcountry-shipping.php
+++ oik-weightcountry-shipping.php
@@ -329,7 +329,7 @@ function init_oik_shipping() {
   class OIK_Shipping2 extends OIK_Shipping {
       function __construct() {
       $this->id           = 'awd_shipping2'; // Retain the original code rather than use 'oik_shipping';
-      $this->method_title = __( 'DHL', 'oik-weightcountry-shipping' );
+      $this->method_title = __( 'Hong Kong Post', 'oik-weightcountry-shipping' );
 
       $this->admin_page_heading     = __( 'Weight and country based shipping', 'oik-weightcountry-shipping' );
       $this->admin_page_description = __( 'Define shipping by weight and country', 'oik-weightcountry-shipping' );
